bd3d8b970a0d90d620b6dbd7517a4bf6a9bc9422
Andrzej97 <szafand@gmail.com>
2020-06-03 16:19:05 +0200

    Added best params searching

--- main.py
+++ main.py
@@ -50,6 +50,38 @@ def find_R_peaks(ecg):
     return r_peaks
 
 
+def find_R_peaks10(ecg):
+    ref_samples = list(ecg[0:REF_SAMPLES])
+    ref_samples_sum = sum(ref_samples)
+    search_samples_left = 0
+    max_signal = 0
+    max_diff_sample = (0, 0)
+    r_peaks = []
+    for sample in enumerate(ecg):
+        if sample[0] < REF_SAMPLES:
+            continue
+        ref_signal = ref_samples_sum / REF_SAMPLES
+        signal = sample[1] - ref_signal
+        if signal > THRESHOLD:
+            if signal > max_signal:
+                max_signal = signal
+                max_diff_sample = sample
+            if search_samples_left <= 0:
+                search_samples_left = SEARCH_SAMPLES
+        if search_samples_left == 1:
+            r_peaks.append(max_diff_sample)
+            if 10 == len(r_peaks):
+                return r_peaks
+            max_signal = 0
+            max_diff_sample = (0, 0)
+        search_samples_left -= 1
+
+        ref_samples_sum -= ref_samples.pop(0)
+        ref_samples_sum += sample[1]
+        ref_samples.append(sample[1])
+    return r_peaks
+
+
 weights = [0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.05, 0.05, 0.05, 0.05]
 PEAK_THRESHOLD = 0.3
 def find_R_peaks_weights(ecg):
@@ -72,10 +104,39 @@ def find_R_peaks_weights(ecg):
             r_peaks.append(max_diff_sample)
             last_ten_peaks.pop()
             last_ten_peaks.insert(0,max_diff_sample)
-            print(expected_peak, max_diff_sample)
+            # print(expected_peak, max_diff_sample)
             max_signal = 0
             max_diff_sample = (0, 0)
             expected_peak = get_expected_peak(last_ten_peaks)
+        search_samples_left -= 1
+
+    return r_peaks
+
+
+def find_R_peaks_weights2(ecg, weight, threshold):
+    old_r_peaks = find_R_peaks10(ecg)
+    last_ten_peaks = old_r_peaks[:10]
+    last_ten_peaks.reverse()
+    r_peaks = []
+    max_signal = 0
+    max_diff_sample = (0,0)
+    search_samples_left = 0
+    expected_peak = get_expected_peak(last_ten_peaks)
+    for sample in enumerate(ecg):
+        if abs(sample[1] - expected_peak) < threshold:
+            if sample[1] > max_signal:
+                max_signal = sample[1]
+                max_diff_sample = sample
+            if search_samples_left <= 0:
+                search_samples_left = SEARCH_SAMPLES
+        if search_samples_left == 1:
+            r_peaks.append(max_diff_sample)
+            last_ten_peaks.pop()
+            last_ten_peaks.insert(0,max_diff_sample)
+            # print(expected_peak, max_diff_sample)
+            max_signal = 0
+            max_diff_sample = (0, 0)
+            expected_peak = (1 - weight) * expected_peak + weight * max_diff_sample[1]
         search_samples_left -= 1
 
     return r_peaks
@@ -122,6 +183,62 @@ def calculate_stats(signal_ch0, annotated_x, detected_x):
             if not in_y_range_found:
                 f_pos.append(det)
     print('t_pos:', len(t_pos), 'f_pos:', len(f_pos), 'f_neg: ', len(f_neg))
+    return t_pos, f_pos, f_neg
+
+def calculate_stats_for_tests(signal_ch0, annotated_x, detected_x):
+    f_neg = 0
+    t_pos = 0
+    print('annotated:', len(annotated_x), '/ detected:', len(detected_x))
+
+    for anno in annotated_x:
+        in_range = list(filter(lambda x: anno - DETECTION_X_RANGE <= x <= anno + DETECTION_X_RANGE, detected_x))
+        in_y_range_found= False
+        if len(in_range) > 0:
+            for x in in_range:
+                if abs(signal_ch0[anno] - signal_ch0[x]) <= DETECTION_Y_RANGE:
+                    t_pos += 1
+                    in_y_range_found = True
+                    break
+            if not in_y_range_found:
+                f_neg += 1
+        else:
+            f_neg += 1
+
+    f_pos = len(detected_x) - t_pos
+    print('t_pos:', t_pos, 'f_pos:', f_pos, 'f_neg: ', f_neg)
+    return t_pos, f_pos, f_neg
+
+def calculate_stats_for_tests_bitmap(signal_ch0, annotated_x, detected_x):
+    f_neg = 0
+    t_pos = 0
+    print('annotated:', len(annotated_x), '/ detected:', len(detected_x))
+
+    bitmap_len = annotated_x[len(annotated_x) - 1] + REF_SAMPLES
+    anno_bitmap = np.zeros(bitmap_len)
+    det_bitmap = np.zeros(bitmap_len)
+    for x in annotated_x:
+        for i in range(0, REF_SAMPLES):
+            if x - i > 0:
+                anno_bitmap[x - i] = 1
+            anno_bitmap[x + i] = 1
+    for x in detected_x:
+        if x < bitmap_len:
+            det_bitmap[x] = 1
+
+    go_to_next_peak = False
+    for i in range(0, bitmap_len):
+        if anno_bitmap[i]:
+            if go_to_next_peak:
+                continue
+            if det_bitmap[i]:
+                t_pos += 1
+                go_to_next_peak = True
+        else:
+            go_to_next_peak = False
+
+    f_neg = len(annotated_x) - t_pos
+    f_pos = len(detected_x) - t_pos
+    print('t_pos:', t_pos, 'f_pos:', f_pos, 'f_neg: ', f_neg)
     return t_pos, f_pos, f_neg
 
 def get_r_samples(ann):
--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import wfdb
 import numpy as np
 import csv
 import main
+from timeit import default_timer as timer
 
 SAMPLES_PER_SECOND = 360
 FILES = ['100', '107', '108', '200', '203', '207', '222', '233']
@@ -15,29 +16,39 @@ def get_real_r_peaks(filename):
     return main.get_r_samples(anno)
 
 
-def get_found_r_peaks(filename):
+def get_found_r_peaks(filename, weight, threshold):
     record = wfdb.rdrecord(filename)
     signal_ch0 = list(map(lambda x: x[0], record.p_signal))
     ecg = np.array(signal_ch0)
-    return main.find_R_peaks(ecg)
+    # return main.find_R_peaks(ecg)
+    return main.find_R_peaks_weights2(ecg, weight, threshold)
 
 
-def test_detection(file_number):
+def test_detection(file_number, weight, threshold):
     filename = 'db/' + file_number
-    found_r_peaks = get_found_r_peaks(filename)
+    start_time = timer()
+    found_r_peaks = get_found_r_peaks(filename, weight, threshold)
+    end_time = timer()
+    # print('get_found_r_peaks took: ', end_time - start_time)
     found_r_peaks_x = list(map(lambda x: x[0], found_r_peaks))
+    start_time = timer()
     real_r_peaks = get_real_r_peaks(filename)
+    end_time = timer()
+    # print('get_real_r_peaks took: ', end_time - start_time)
     real_r_peaks = filter_real_r_peaks(real_r_peaks, file_number)
     real_r_peaks_x = list(map(lambda x: x[0], real_r_peaks))
-    print('file: ' + filename)
+    # print('file: ' + filename)
     record = wfdb.rdrecord(filename)
     signal_ch0 = list(map(lambda x: x[0], record.p_signal))
-    t_pos, f_pos, f_neg = main.calculate_stats(signal_ch0, real_r_peaks_x, found_r_peaks_x)
-    return len(real_r_peaks), len(t_pos), len(f_pos), len(f_neg)
+    start_time = timer()
+    t_pos, f_pos, f_neg = main.calculate_stats_for_tests_bitmap(signal_ch0, real_r_peaks_x, found_r_peaks_x)
+    end_time = timer()
+    # print('calculate_stats took: ', end_time - start_time)
+    return len(real_r_peaks), t_pos, f_pos, f_neg
 
 
-def write_stats_row(writer, file, anno, t_pos, f_pos, f_neg):
-    writer.writerow([file, anno, t_pos, "{:.4f}".format(t_pos / anno), f_pos, "{:.4f}".format(f_pos / anno),
+def write_stats_row(writer, file, weight, threshold, anno, t_pos, f_pos, f_neg):
+    writer.writerow([file, weight, threshold, anno, t_pos, "{:.4f}".format(t_pos / anno), f_pos, "{:.4f}".format(f_pos / anno),
                      f_neg, "{:.4f}".format(f_neg / anno)])
 
 
@@ -86,20 +97,28 @@ def get_msec(time):
 if __name__ == '__main__':
     # freeze_support()
     # main.download_all_files()
+    start_time = timer()
     total_R_annotations = 0
     total_positive = 0
     total_false_positive = 0
     total_false_negative = 0
-    with open('results.csv', 'w', newline='') as csvfile:
+    with open('resultsParams.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(['File', 'R_annotations', 'Positive', '%ofPositive', 'FalsePositive', '%OfFalsePositive',
+        writer.writerow(['File', 'Prev_weight', 'Y_threshold', 'R_annotations', 'Positive', '%ofPositive', 'FalsePositive', '%OfFalsePositive',
                          'FalseNegative', '%OfFalseNegative'])
-        for file in FILES:
-            anno, t_pos, f_pos, f_neg = test_detection(file)
-            write_stats_row(writer, file, anno, t_pos, f_pos, f_neg)
-            total_R_annotations += anno
-            total_positive += t_pos
-            total_false_positive += f_pos
-            total_false_negative += f_neg
-        write_stats_row(writer, 'TOTAL', total_R_annotations, total_positive, total_false_positive,
-                        total_false_negative)
+        weights = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+        y_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+        for weight in weights:
+            for threshold in y_thresholds:
+                print('weight: ' + str(weight) + ', threshold:' + str(threshold))
+                for file in FILES:
+                    anno, t_pos, f_pos, f_neg = test_detection(file, weight, threshold)
+                    # write_stats_row(writer, file, weight, threshold, anno, t_pos, f_pos, f_neg)
+                    total_R_annotations += anno
+                    total_positive += t_pos
+                    total_false_positive += f_pos
+                    total_false_negative += f_neg
+                write_stats_row(writer, 'TOTAL', weight, threshold, total_R_annotations, total_positive, total_false_positive,
+                                total_false_negative)
+    end_time = timer()
+    print('Testing time: ' + str(end_time - start_time) + ' seconds')
