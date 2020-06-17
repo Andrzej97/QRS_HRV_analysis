16e51d7375e46071d9919c0c2c1432021ab7d2af
Szymon Bernal <szymon.bernal@gmail.com>
2020-06-17 21:23:32 +0200

    Refactor test.py

--- tests.py
+++ tests.py
@@ -1,54 +1,52 @@
 from multiprocessing.dummy import freeze_support
 import wfdb
 import numpy as np
-import csv
 import main
-from timeit import default_timer as timer
+import json
+import functools
+import time
 
 SAMPLES_PER_SECOND = 360
+# FILES = ['233']
 FILES = ['100', '107', '108', '200', '203', '207', '222', '233']
 FILES_FRAGMENTS = {'100': [('00:00:00.000', '00:30:05.600')],
                    '207': [('00:00:00.000', '00:29:08.500')]}
 
 
-def get_real_r_peaks(anno):
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"{func.__name__!r} took {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+def get_r_peaks_from(anno):
     return main.get_r_samples(anno)
 
+# @timer
+def get_found_r_peaks(record_signal_ch0, weight, threshold):
+    # return main.find_R_peaks(record_signal_ch0)
+    return main.find_R_peaks_weights2(record_signal_ch0, weight, threshold)
 
-def get_found_r_peaks(record, weight, threshold):
-    signal_ch0 = list(map(lambda x: x[0], record.p_signal))
-    ecg = np.array(signal_ch0)
-    # return main.find_R_peaks(ecg)
-    return main.find_R_peaks_weights2(ecg, weight, threshold)
+# @timer
+def calculate_stats_for_tests_bitmap(anno_r_peaks_x, found_r_peaks_x):
+     return main.calculate_stats_for_tests_bitmap(anno_r_peaks_x, found_r_peaks_x)
 
+def test_detection(file_number, record_signal_ch0, anno_r_peaks_x, weight, threshold):
+    found_r_peaks = get_found_r_peaks(record_signal_ch0, weight, threshold)
 
-def test_detection(file_number, record, anno, weight, threshold):
-    start_time = timer()
-    found_r_peaks = get_found_r_peaks(record, weight, threshold)
-    end_time = timer()
-    # print('get_found_r_peaks took: ', end_time - start_time)
-    # TODO: filter below peaks by FILES_FRAGMENTS
+    found_r_peaks = filter_r_peaks(found_r_peaks, file_number)
     found_r_peaks_x = list(map(lambda x: x[0], found_r_peaks))
-    start_time = timer()
-    real_r_peaks = get_real_r_peaks(anno)
-    end_time = timer()
-    # print('get_real_r_peaks took: ', end_time - start_time)
-    real_r_peaks = filter_real_r_peaks(real_r_peaks, file_number)
-    real_r_peaks_x = list(map(lambda x: x[0], real_r_peaks))
-    # print('file: ' + filename)
-    start_time = timer()
-    t_pos, f_pos, f_neg = main.calculate_stats_for_tests_bitmap(real_r_peaks_x, found_r_peaks_x)
-    end_time = timer()
-    # print('calculate_stats took: ', end_time - start_time)
-    return len(real_r_peaks), t_pos, f_pos, f_neg
 
+    t_pos, f_pos, f_neg = calculate_stats_for_tests_bitmap(anno_r_peaks_x, found_r_peaks_x)
+    return t_pos, f_pos, f_neg
 
-def write_stats_row(writer, file, weight, threshold, anno, t_pos, f_pos, f_neg):
-    writer.writerow([file, weight, threshold, anno, t_pos, "{:.4f}".format(t_pos / anno), f_pos, "{:.4f}".format(f_pos / anno),
-                     f_neg, "{:.4f}".format(f_neg / anno)])
-
-
-def filter_real_r_peaks(real_r_peaks, filenumber):
+def filter_r_peaks(real_r_peaks, filenumber):
     if filenumber not in list(FILES_FRAGMENTS.keys()):
         return real_r_peaks
     filtered_r_peaks = []
@@ -90,34 +88,60 @@ def get_sec(time):
 def get_msec(time):
     return int(time[9:12])
 
-if __name__ == '__main__':
-    # freeze_support()
-    # main.download_all_files()
-    start_time = timer()
-    total_R_annotations = 0
-    total_positive = 0
-    total_false_positive = 0
-    total_false_negative = 0
-    with open('resultsParams.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(['File', 'Prev_weight', 'Y_threshold', 'R_annotations', 'Positive', '%ofPositive', 'FalsePositive', '%OfFalsePositive',
-                         'FalseNegative', '%OfFalseNegative'])
-        weights = [0.2]
-        y_thresholds = [0.1]
+def create_stats(weight, threshold, t_pos, f_pos, f_neg):
+    return {
+        'weight': weight,
+        'threshold': threshold,
+        'stats': {
+            'true_positive': t_pos,
+            'false_positive': f_pos,
+            'false_negative': f_neg
+        }
+    }
+
+@timer
+def test_all(weights, y_thresholds):
+    # Map<Filename, List<Result>>
+    statistics = { filename : [] for filename in FILES }
+
+    for file in FILES:
+        filename = 'db/' + file
+        record = wfdb.rdrecord(filename)
+        record_signal_ch0 = list(map(lambda x: x[0], record.p_signal))
+
+        anno = wfdb.rdann(filename, 'atr')
+        anno_r_peaks = get_r_peaks_from(anno)
+        anno_r_peaks = filter_r_peaks(anno_r_peaks, file)
+        anno_r_peaks_x = list(map(lambda x: x[0], anno_r_peaks))
+        annotation_count = len(anno_r_peaks)
+
+        file_stats = {
+            'filename': file,
+            'annotation_count': annotation_count,
+            'measurements': []
+        }
+
         for weight in weights:
             for threshold in y_thresholds:
                 print('weight: ' + str(weight) + ', threshold:' + str(threshold))
-                for file in FILES:
-                    filename = 'db/' + file
-                    record = wfdb.rdrecord(filename)
-                    anno = wfdb.rdann(filename, 'atr')
-                    annotations_len, t_pos, f_pos, f_neg = test_detection(file, record, anno, weight, threshold)
-                    # write_stats_row(writer, file, weight, threshold, anno, t_pos, f_pos, f_neg)
-                    total_R_annotations += annotations_len
-                    total_positive += t_pos
-                    total_false_positive += f_pos
-                    total_false_negative += f_neg
-                write_stats_row(writer, 'TOTAL', weight, threshold, total_R_annotations, total_positive, total_false_positive,
-                                total_false_negative)
-    end_time = timer()
-    print('Testing time: ' + str(end_time - start_time) + ' seconds')
+                t_pos, f_pos, f_neg = test_detection(file, record_signal_ch0, anno_r_peaks_x, weight, threshold)
+                file_stats['measurements'].append(create_stats(weight, threshold, t_pos, f_pos, f_neg))
+
+        statistics[file] = file_stats
+
+    return statistics
+
+if __name__ == '__main__':
+    # freeze_support()
+    # main.download_all_files()
+    
+    # weights = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+    # y_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    weights = [0.2]
+    y_thresholds = [0.1]
+
+    stats = test_all(weights, y_thresholds)
+
+    f = open('result.json', 'w')
+    f.write(json.dumps(stats))
+    f.close()
