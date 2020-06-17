62f6216e832e26eeabbf2a02006a15447180746c
Szymon Bernal <szymon.bernal@gmail.com>
2020-06-17 21:58:39 +0200

    Add parallel version

--- tests.py
+++ tests.py
@@ -1,29 +1,18 @@
 from multiprocessing.dummy import freeze_support
+from multiprocessing import Pool
+from timer import timer
 import wfdb
 import numpy as np
 import main
 import json
-import functools
-import time
 
 SAMPLES_PER_SECOND = 360
-# FILES = ['233']
 FILES = ['100', '107', '108', '200', '203', '207', '222', '233']
 FILES_FRAGMENTS = {'100': [('00:00:00.000', '00:30:05.600')],
                    '207': [('00:00:00.000', '00:29:08.500')]}
 
-
-def timer(func):
-    """Print the runtime of the decorated function"""
-    @functools.wraps(func)
-    def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()
-        value = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        run_time = end_time - start_time
-        print(f"{func.__name__!r} took {run_time:.4f} secs")
-        return value
-    return wrapper_timer
+WEIGHTS = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+Y_THRESHOLDS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
 
 def get_r_peaks_from(anno):
     return main.get_r_samples(anno)
@@ -99,49 +88,52 @@ def create_stats(weight, threshold, t_pos, f_pos, f_neg):
         }
     }
 
+def test_file(file):
+    filename = 'db/' + file
+    record = wfdb.rdrecord(filename)
+    record_signal_ch0 = list(map(lambda x: x[0], record.p_signal))
+
+    anno = wfdb.rdann(filename, 'atr')
+    anno_r_peaks = get_r_peaks_from(anno)
+    anno_r_peaks = filter_r_peaks(anno_r_peaks, file)
+    anno_r_peaks_x = list(map(lambda x: x[0], anno_r_peaks))
+    annotation_count = len(anno_r_peaks)
+
+    file_stats = {
+        'filename': file,
+        'annotation_count': annotation_count,
+        'measurements': []
+    }
+
+    for weight in WEIGHTS:
+        for threshold in Y_THRESHOLDS:
+            print('weight: ' + str(weight) + ', threshold:' + str(threshold))
+            t_pos, f_pos, f_neg = test_detection(file, record_signal_ch0, anno_r_peaks_x, weight, threshold)
+            file_stats['measurements'].append(create_stats(weight, threshold, t_pos, f_pos, f_neg))
+
+    return file_stats
+
 @timer
-def test_all(weights, y_thresholds):
-    # Map<Filename, List<Result>>
-    statistics = { filename : [] for filename in FILES }
-
+def test_all_single_thr():
+    stats = []
     for file in FILES:
-        filename = 'db/' + file
-        record = wfdb.rdrecord(filename)
-        record_signal_ch0 = list(map(lambda x: x[0], record.p_signal))
+        stats.append(test_file(file))
 
-        anno = wfdb.rdann(filename, 'atr')
-        anno_r_peaks = get_r_peaks_from(anno)
-        anno_r_peaks = filter_r_peaks(anno_r_peaks, file)
-        anno_r_peaks_x = list(map(lambda x: x[0], anno_r_peaks))
-        annotation_count = len(anno_r_peaks)
+    return stats
 
-        file_stats = {
-            'filename': file,
-            'annotation_count': annotation_count,
-            'measurements': []
-        }
+@timer
+def test_all_multi_thr():
+    res = []
+    with Pool(8) as p:
+        res = p.map(test_file, FILES)
 
-        for weight in weights:
-            for threshold in y_thresholds:
-                print('weight: ' + str(weight) + ', threshold:' + str(threshold))
-                t_pos, f_pos, f_neg = test_detection(file, record_signal_ch0, anno_r_peaks_x, weight, threshold)
-                file_stats['measurements'].append(create_stats(weight, threshold, t_pos, f_pos, f_neg))
-
-        statistics[file] = file_stats
-
-    return statistics
+    return res
 
 if __name__ == '__main__':
-    # freeze_support()
     # main.download_all_files()
+    # res = test_all_single_thr()
+    res = test_all_multi_thr()
     
-    # weights = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
-    # y_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-    weights = [0.2]
-    y_thresholds = [0.1]
-
-    stats = test_all(weights, y_thresholds)
-
     f = open('result.json', 'w')
-    f.write(json.dumps(stats))
+    f.write(json.dumps(res))
     f.close()
